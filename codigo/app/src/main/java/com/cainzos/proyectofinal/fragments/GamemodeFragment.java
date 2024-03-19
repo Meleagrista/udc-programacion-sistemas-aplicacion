a6bceba1ef1f7a70bf950d2bc69f742299b5180a
Alvaro Cainzos <alvaro.cainzos@udc.es>
2024-03-19 18:43:35 +0100

    Arreglado problema con el fragmento de gamemode

--- codigo/app/src/main/java/com/cainzos/proyectofinal/fragments/GamemodeFragment.java
+++ codigo/app/src/main/java/com/cainzos/proyectofinal/fragments/GamemodeFragment.java
@@ -35,15 +35,18 @@ public class GamemodeFragment extends Fragment {
         binding = FragmentGamemodeBinding.inflate(inflater, container, false);
         View rootView = binding.getRoot();
 
+        int left = 20;
+        int right = 20;
+
         /*---Gestion de las previews en el lateral de cada viewPage---*/
         viewPager = binding.viewPager;
         adapter = new CustomAdapter();
         viewPager.setAdapter(adapter);
         viewPager.setClipToPadding(false);
-        viewPager.setPadding(R.dimen.left_padding_viewpager, 0, R.dimen.right_padding_viewpager, 0);
+        viewPager.setPadding(left, 0, right, 0);
         viewPager.setOffscreenPageLimit(1);
-        final float nextItemVisiblePx = R.dimen.viewpager_next_item_visible;
-        final float currentItemHorizontalMarginPx = R.dimen.viewpager_current_item_horizontal_margin;
+        final float nextItemVisiblePx = getResources().getDimension(R.dimen.viewpager_next_item_visible);
+        final float currentItemHorizontalMarginPx =getResources().getDimension(R.dimen.viewpager_current_item_horizontal_margin);
         final float pageTranslationX = nextItemVisiblePx + currentItemHorizontalMarginPx;
 
         /*---Modificar la apariencia de los previews---*/
